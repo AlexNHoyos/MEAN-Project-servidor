390ed7310f90ed565a0fa1de2abf718be725d9b6
Alex <alexnicolashoyos@gmail.com>
2024-05-20 22:28:45 -0300

    Agregado conexion db

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+//Conectar db
+
+const conectarDB = async() => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParse: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+        console.log('DB Conectada')
+    } catch (error) {
+        console.log(error);
+        process.exit(1); //Detengo app
+    }
+}
+
+module.exports() = conectarDB
