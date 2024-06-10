96032e8e39c5a61481e1401e750064b52cd922a5
Alex <alexnicolashoyos@gmail.com>
2024-06-10 18:57:15 -0300

    Agregado producto.js

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const app = express();
 //Conectamos a la DB
 conectarDB();
 
+app.use('/api/productos', require('./routes/producto'))
+
 //Defino ruta principal
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.send('Hola mundo');
-})
+})*/
 
 app.listen(4000, () => {
     console.log('El servidor está corriendo perfectamente')
