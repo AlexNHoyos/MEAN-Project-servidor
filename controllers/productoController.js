6b8e9e5eeb21f1d6592f924489f77e29112208f1
Alex <alexnicolashoyos@gmail.com>
2024-06-14 00:33:29 -0300

    Agregado findall y update

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -14,3 +14,38 @@ exports.crearProducto = async(req, res) =>{
         res.status(500).send('Hubo un error inesperado');
     }
 }
+
+exports.obtenerProducto = async (req, res) =>{
+    try {
+    //Traemos todos los productos
+        const productoss = await Producto.find();
+        res.json(productoss);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error inesperado');
+    }
+}
+
+exports.actualizarProducto = async (req, res) =>{
+    try {
+        const { nombre, categoria, ubicacion, precio } = req.body;
+        let producto = await Producto.findById(req.params.id);
+        if (!producto){
+            res.status(404).json({msg: 'No existe el producto'})
+        }
+
+        producto.nombre = nombre;
+        producto.categoria = categoria;
+        producto.ubicacion = ubicacion;
+        producto.precio = precio;
+
+        producto = await Producto.findOneAndUpdate({_id: req.params.id}, producto, {new:true});
+        
+        res.json();
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error inesperado');
+    }
+}
--- routes/producto.js
+++ routes/producto.js
@@ -4,5 +4,8 @@ const router =express.Router();
 const productoController = require('../controllers/productoController');
 
 //api/productos
-router.post('/', productoController.crearProducto)
+router.post('/', productoController.crearProducto);
+router.get('/',productoController.obtenerProducto);
+router.put('/:id', productoController.actualizarProducto);
+
 module.exports = router;
